b7385d2979bcaae6e76a17fc609fd5d697667b22
Aditya Yadav <97939828+OpAditya1109@users.noreply.github.com>
2024-11-18 00:39:56 +0530

    Update assets.js

--- Secura-Pay/src/assets/assets.js
+++ Secura-Pay/src/assets/assets.js
@@ -3,7 +3,7 @@ import grofers from "./grofers.jpg";
 import logo2 from "./logo.png";
 import oyo from "./oyo.png";
 import profile from "./profile.png";
-
+import close from "./close-line.png";
 
 export const assets = {
   logo,
@@ -11,5 +11,6 @@ export const assets = {
   logo2,
   oyo,
   profile,
+  close,
   
 };
